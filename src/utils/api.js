23e89f21ffd973ec2c146e3135a7233f602c5bcb
alaajerbi <mralaajerbi@gmail.com>
2019-01-15 14:41:42 +0100

    Make api requests over https

--- src/utils/api.js
+++ src/utils/api.js
@@ -14,16 +14,15 @@ export function fetchStoriesByKeyword(keyword, page = 0) {
 }
 
 function fetchFrontPageStories(page = 0) {
-  const url = window.encodeURI('http://hn.algolia.com/api/v1/search?tags=front_page&page=' + page + '&hitsPerPage=50');
+  const url = window.encodeURI('https://hn.algolia.com/api/v1/search?tags=front_page&page=' + page + '&hitsPerPage=50');
   return fetchStoriesFromUrl(url);
 }
 
 //fetch stories by tag
 export function fetchStoriesByTag(tag, page = 0) {
-  console.log(page);
   if (tag === 'front_page') {
     return fetchFrontPageStories(page);
   }
-  const url = window.encodeURI('http://hn.algolia.com/api/v1/search_by_date?tags=' + tag + '&page=' + page + '&hitsPerPage=50');
+  const url = window.encodeURI('https://hn.algolia.com/api/v1/search_by_date?tags=' + tag + '&page=' + page + '&hitsPerPage=50');
   return fetchStoriesFromUrl(url);
 }
