4705a8f3f82ba04bc485efe926dec0317939868f
alaajerbi <mralaajerbi@gmail.com>
2019-01-15 14:18:50 +0100

    Updates

---
+++ precache-manifest.a2ca42ffe48c338f4e568372894e3a6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "230cf5b77aab47318f4f",
+    "url": "/hackernews-clone/static/js/runtime~main.230cf5b7.js"
+  },
+  {
+    "revision": "ed57a0c1f69f454cfb6d",
+    "url": "/hackernews-clone/static/js/main.ed57a0c1.chunk.js"
+  },
+  {
+    "revision": "2f78b54ca8fa7262bd69",
+    "url": "/hackernews-clone/static/js/1.2f78b54c.chunk.js"
+  },
+  {
+    "revision": "ed57a0c1f69f454cfb6d",
+    "url": "/hackernews-clone/static/css/main.4dd54700.chunk.css"
+  },
+  {
+    "revision": "2f78b54ca8fa7262bd69",
+    "url": "/hackernews-clone/static/css/1.c56aae82.chunk.css"
+  },
+  {
+    "revision": "ad2130f461fdcae482a13ad9d6299598",
+    "url": "/hackernews-clone/index.html"
+  }
+];
